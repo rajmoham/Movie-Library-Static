dd32e16322cd7347270565e572f9c08ec27f84ac
rajmoham <rajmoham10@gmail.com>
2022-10-14 11:44:09 +0100

    finished project

---
+++ index.js
@@ -0,0 +1,11 @@
+function openMenu()
+{
+    document.getElementById("body").style.overflow = "hidden";
+    document.getElementById("menu").style.display = "flex";
+}
+
+function closeMenu()
+{
+    document.getElementById("body").style.overflow = "auto";
+    document.getElementById("menu").style.display = "none";
+}
